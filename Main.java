27893f06ecf086ef01a59be5cf3695bafaef7157
hiiamcailyn <152474332+hiiamcailyn@users.noreply.github.com>
2025-02-21 19:03:15 +0000

    yes

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;import java.util.Scanner;
-public class Main{  public static void main(String[] args) 
-{   
+public class Main{public static void main(String[] args)
+{
   // ArrayList<String> dingtea = new ArrayList<String>();// dingtea.add("A");   
   // dingtea.add("B");  
   // dingtea.add("C");  
@@ -12,35 +12,35 @@ public class Main{  public static void main(String[] args) 
   // System.out.println("the list shift left "+dingtea);
 // Part 2  
   ArrayList<Integer> dingteas = new ArrayList<Integer>();
-  dingtea.add(2); 
+  dingtea.add(2);
   dingtea.add(5);
-  dingtea.add(7); 
+  dingtea.add(7);
   dingtea.add(5);
-  System.out.println("This is the list of boba teas" + dingteas); 
+ System.out.println("This is the list of boba teas" + dingteas);
   System.out.println("This is the stats of boba teas" + printStatics(dingteas));
-  }
-  public static void shiftLeft(ArrayList<String> arr)
-  {    System.out.println("ArrayList before doing anything" + arr);    //A
-   ssume array has one element 
-    String front = arr.remove(0);
+ }
+ public static void shiftLeft(ArrayList<String> arr)
+  {
+     System.out.println("ArrayList before doing anything" + arr);//Assume array has one element 
+  String front = arr.remove(0);
    // this will shift everthing to the left  
    arr.add(front);
-  }
-  public static void printStatics(ArrayList<Integer> arr) 
-  {   
-    int count = 0;  
-   int sum = 0;   
-   int mode = 0;   
+ }
+ public static void printStatics(ArrayList<Integer> arr) 
+ {
+    int count = 0;
+   int sum = 0;
+   int mode = 0;
    int average = 0; 
    for (int i = 0; i < dingteas.size(); i++) 
-     {     
-       sum += i;     
+     {
+       sum += i;
       count ++; 
-    }   
-   System.out.println("Sum: " + sum);   
-   average = sum/ count;   
-   System.out.println("Average: " + average); 
-   mode = sum % count;  
+}
+   System.out.println("Sum: " + sum);
+   average = sum/ count; 
+   System.out.println("Average: " + average);
+   mode = sum % count;
    System.out.println("Mode: " + mode);
   }
     }
