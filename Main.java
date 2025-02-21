7e1c60d89cb25d9010ebb59a0a1da7fb1dd694cb
hiiamcailyn <152474332+hiiamcailyn@users.noreply.github.com>
2025-02-21 10:48:51 -0800

    Update Main.java
    wed

--- Main.java
+++ Main.java
@@ -1,20 +1,44 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class Main
-{
-  public static void main(String[] args)
-  {
-    // Feel free to test code here
+import java.util.ArrayList;import java.util.Scanner;
+public class Main{  public static void main(String[] args) 
+{   
+  // ArrayList<String> dingtea = new ArrayList<String>();// dingtea.add("A");   
+  // dingtea.add("B");  
+  // dingtea.add("C");  
+  // dingtea.add("D");  
+  // dingtea.add("E"); 
+  // dingtea.add("F");   
+  // System.out.println("the list "+ dingtea);  
+  // shiftLeft(dingtea);  
+  // System.out.println("the list shift left "+dingtea);
+// Part 2  
+  ArrayList<Integer> dingteas = new ArrayList<Integer>();
+  dingtea.add(2); 
+  dingtea.add(5);
+  dingtea.add(7); 
+  dingtea.add(5);
+  System.out.println("This is the list of boba teas" + dingteas); 
+  System.out.println("This is the stats of boba teas" + printStatics(dingteas));
+  }
+  public static void shiftLeft(ArrayList<String> arr)
+  {    System.out.println("ArrayList before doing anything" + arr);    //A
+   ssume array has one element 
+    String front = arr.remove(0);
+   // this will shift everthing to the left  
+   arr.add(front);
+  }
+  public static void printStatics(ArrayList<Integer> arr) 
+  {    int count = 0;  
+   int sum = 0;   
+   int mode = 0;   
+   int average = 0; 
+   for (int i = 0; i < dingteas.size(); i++) 
+     {      sum += i;     
+      count ++; 
+    }   
+   System.out.println("Sum: " + sum);   
+   average = sum/ count;   
+   System.out.println("This is the average amount of dingteas " + average); 
+   mode = sum % count;  
+   System.out.println("This is the mode of the aount of dingteas " + mode);
   }
-
-  public static void shiftLeft(ArrayList<String> arr)
-  {
-    
-  }
-
-  public static printStatistics(final ArrayList<Integer> arr)
-  {
-    
-  }
-}
+    }
